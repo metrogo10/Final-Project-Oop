4847e328c6022572059ec7622bb4c93537f6d612
Lay <tonglay1219@gmail.com>
2017-05-24 15:26:35 -0600

    Add WF file, Main and deleted useless file

---
+++ FinalProject/FinalProject/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public class Driver
+    {
+        public static void Main()
+        {
+            Application.Run(new MainWindow());
+        }
+    }
+}
