c7432270dc2976b64cb6259e52e5f8883fa1fe42
Addison Jones <adjones@student.neumont.edu>
2017-06-02 16:30:43 -0600

    fix main window

--- wpf version/Proteus/Proteus/MainWindow.xaml.cs
+++ wpf version/Proteus/Proteus/MainWindow.xaml.cs
@@ -18,9 +18,6 @@ using System.Windows.Shapes;
 using Proteus.Engines;
 namespace Proteus
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -31,32 +28,11 @@ namespace Proteus
         {
             NewGame secondWindow = new NewGame();
             secondWindow.Show();
+            this.Owner.Content.Equals(secondWindow.Content);
         }
         private void LoadGame(object sender, RoutedEventArgs e)
         {
            
         }
     }
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-		private void NewGame(object sender, RoutedEventArgs e)
-		{
-			NewGame secondWindow = new NewGame();
-			secondWindow.Show();
-		}
-		private void LoadGame(object sender, RoutedEventArgs e)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "Character Templates (*.ptemp)|*.ptemp";
-			if (openFileDialog.ShowDialog() == DialogResult)
-				MainEngine.Template = FileIO.LoadCharacter(openFileDialog.FileName);
-		}
-	}
 }
